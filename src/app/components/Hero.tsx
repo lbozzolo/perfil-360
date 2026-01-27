06bf31e49a194b4deac1b97a505a3311936a9009
Lucas Bozzolo <lbozzolo@users.noreply.github.com>
2026-01-27 09:50:45 -0300

    Actualizar video de la home con sonido activado

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -22,7 +22,7 @@ export default function Hero() {
             <iframe 
               width="100%" 
               height="100%" 
-              src="https://www.youtube.com/embed/u31qwQUeGuM?autoplay=1" 
+              src="https://www.youtube.com/embed/PdaeM1se7aM?autoplay=1&mute=0" 
               title="YouTube video player" 
               frameBorder="0" 
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
